d0478484b56e7a9f45d34a003f6bcabc38232bd6
Mikołaj <95126739+MikolajKos@users.noreply.github.com>
2022-11-18 15:48:05 +0100

    Add files via upload

---
+++ CSharpExamples/CSharpExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
